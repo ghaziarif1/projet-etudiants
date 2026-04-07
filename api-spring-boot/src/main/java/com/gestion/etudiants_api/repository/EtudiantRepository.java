0ada7173daa658c859cb50f457fa5f775fb26442
Ghazi Arif <ghaziarif2003@gmail.com>
2026-04-07 15:55:07 +0100

    PROJ-9 : requête personnalisée findByAnneePremiereInscription

---
+++ api-spring-boot/src/main/java/com/gestion/etudiants_api/repository/EtudiantRepository.java
@@ -0,0 +1,17 @@
+package com.gestion.etudiants_api.repository;
+
+import com.gestion.etudiants_api.entity.Etudiant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EtudiantRepository extends JpaRepository<Etudiant, Long> {
+
+    // Q9 : requête dérivée (Spring Data JPA)
+    List<Etudiant> findByAnneePremiereInscription(int annee);
+
+    // Bonus (utile pour le CRUD) :
+    // List<Etudiant> findByDepartementId(Long departementId);
+}
