bda6019875c0f820162c6a67633d3fd49ff02d4a
Ghazi Arif <ghaziarif2003@gmail.com>
2026-04-07 14:29:53 +0100

    PROJ-2 : ajout méthode age() dans entité Etudiant

--- api-spring-boot/src/main/java/com/gestion/etudiants_api/Etudiant.java
+++ api-spring-boot/src/main/java/com/gestion/etudiants_api/Etudiant.java
@@ -1,40 +1,42 @@
 package com.gestion.etudiants_api;
 
-import jakarta.persistence.*;
 import java.time.LocalDate;
+import java.time.Period;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "etudiants")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Etudiant {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true, length = 8)
     private String cin;
-
-    @Column(nullable = false, length = 100)
     private String nom;
-
-    @Column(name = "date_naissance")
     private LocalDate dateNaissance;
 
-    // Constructeurs
-    public Etudiant() {}
+    // CONSTRUCTEUR AJOUTÉ
     public Etudiant(String cin, String nom, LocalDate dateNaissance) {
         this.cin = cin;
         this.nom = nom;
         this.dateNaissance = dateNaissance;
     }
 
-    // Getters & Setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public String getCin() { return cin; }
-    public void setCin(String cin) { this.cin = cin; }
-    public String getNom() { return nom; }
-    public void setNom(String nom) { this.nom = nom; }
-    public LocalDate getDateNaissance() { return dateNaissance; }
-    public void setDateNaissance(LocalDate dateNaissance) { this.dateNaissance = dateNaissance; }
+    public int age() {
+        return Period.between(this.dateNaissance, LocalDate.now()).getYears();
+    }
 }
